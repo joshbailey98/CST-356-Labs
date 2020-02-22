4b47d6a77f82eb26a5a3b92e67bcfb958f7b1b96
Josh Bailey <josh.bailey@oit.edu>
2020-02-22 12:22:37 -0800

    week 7

---
+++ Week 7/webapi/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Database;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {       
+        private readonly SchoolContext _dbContext;
+
+        public StudentController(SchoolContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            var result = _dbContext.Student.ToList();
+            return Ok(result);
+        }
+    }
+}
---
+++ Week 7/webapp/src/components/Instructors.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <br />
+    <table>
+        <thead>
+            <tr>
+                <th class="heading" colspan="3">Instructors</th>
+            </tr>
+            <tr>
+              <th>First Name</th>
+              <th>Middle Initial</th>
+              <th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody id="instructor-list">
+            <tr v-for="instructor in instructors" v-bind:key="instructor">
+                <td>{{ instructor.first }}</td>
+                <td>{{ instructor.middle }}</td>
+                <td>{{ instructor.last }}</td>
+            </tr>
+        </tbody>
+      </table>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Instructors',
+        mounted() {
+            this.instructors = getInstructors();
+        },
+        data () {
+            return {
+                instructors: []
+            }
+        }
+    }
+    function getInstructors() {
+        return JSON.parse(instructors);
+    }
+    var instructors = '[{"first": "Jill", "middle": "R.", "last": "Smith"}, {"first": "Michael", "middle": "B.", "last": "James"}, {"first": "Tom", "middle": "D.", "last": "Brown"}, {"first": "Bob", "middle": "K.", "last": "Roberts"}, {"first": "Sarah", "middle": "M.", "last": "Jane"}]';
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
